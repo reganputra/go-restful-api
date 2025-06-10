b5d91147fa35b2c6263178f276dbe4da8b96bf07
reganputra <ramadhanregan334@gmail.com>
2025-06-10 14:20:14 +0700

    feat: set up main application with category routes and database connection

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+	"go-restful-api/connection"
+	"go-restful-api/controllers"
+	"go-restful-api/repository"
+	"go-restful-api/service"
+)
+
 func main() {
 
+	db := connection.DatabaseConnect()
+	validate := validator.New()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controllers.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
 }
