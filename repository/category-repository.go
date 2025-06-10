925ac6fef4433bd79140381dcc1dd2ee07764eef
reganputra <ramadhanregan334@gmail.com>
2025-06-10 08:56:23 +0700

    refactor: update FindById method to return entity.Category and error

--- repository/category-repository.go
+++ repository/category-repository.go
@@ -10,6 +10,6 @@ type CategoryRepository interface {
 	Save(ctx context.Context, tx sql.Tx, category entity.Category) entity.Category
 	Update(ctx context.Context, tx sql.Tx, category entity.Category) entity.Category
 	Delete(ctx context.Context, tx sql.Tx, category entity.Category)
-	FindById(ctx context.Context, tx sql.Tx, categoryId int)
+	FindById(ctx context.Context, tx sql.Tx, categoryId int) (entity.Category, error)
 	FindAll(ctx context.Context, tx sql.Tx) []entity.Category
 }
