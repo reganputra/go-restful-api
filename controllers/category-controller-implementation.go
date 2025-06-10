4cba6e58ed41fc03d438e00314ea717f9ba584de
reganputra <ramadhanregan334@gmail.com>
2025-06-10 10:37:23 +0700

    feat: add CategoryController interface and implementation for category operations

---
+++ controllers/category-controller-implementation.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+type CategoryControllerImplementation struct {
+}
+
+func (c *CategoryControllerImplementation) Create(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+}
+
+func (c *CategoryControllerImplementation) Update(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+}
+
+func (c *CategoryControllerImplementation) Delete(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+}
+
+func (c *CategoryControllerImplementation) FindById(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+}
